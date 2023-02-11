09f216ed77438c377c26ebb55700d0ecedf035f1
gugacarbo <guga_carbo@hotmail.com>
2023-02-11 17:33:41 -0300

    with styled component class

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import styled from 'styled-components'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -17,9 +18,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <StyledButton onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </StyledButton>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -30,5 +31,9 @@ function App() {
     </div>
   )
 }
+
+const StyledButton = styled.div`
+  background-color: #09c;
+`
 
 export default App
--- test/src/main.jsx
+++ test/src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const RootElement = document.getElementById('root')
+
+if(RootElement.hasChildNodes()){
+  ReactDOM.hydrateRoot(RootElement,<App />)
+}else{
+  ReactDOM.createRoot(RootElement).render(<App />)
+}
